8019cb0caa8811c1f598d2af761b1873606a4761
Don Miley <donmiley13@gmail.com>
2023-04-24 13:13:41 -0400

    HTML Code

---
+++ menu.php
@@ -0,0 +1,106 @@
+<form>
+  <fieldset>
+    <legend>Pizza</legend>
+    <label>
+      Size:
+      <input type="radio" name="pizza-size" value="small">
+      Small
+      <input type="radio" name="pizza-size" value="medium">
+      Medium
+      <input type="radio" name="pizza-size" value="large">
+      Large
+    </label>
+    <br>
+    <label>
+      Toppings:
+      <input type="checkbox" name="topping" value="pepperoni">
+      Pepperoni
+      <input type="checkbox" name="topping" value="bacon">
+      Bacon
+      <input type="checkbox" name="topping" value="sausage">
+      Sausage
+      <input type="checkbox" name="topping" value="pineapple">
+      Pineapple
+      <input type="checkbox" name="topping" value="onions">
+      Onions
+    </label>
+    <br>
+    <label>
+      Quantity:
+      <select name="pizza-quantity">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </label>
+  </fieldset>
+  <fieldset>
+    <legend>French Fries</legend>
+    <label>
+      Size:
+      <input type="radio" name="fries-size" value="small">
+      Small
+      <input type="radio" name="fries-size" value="large">
+      Large
+    </label>
+    <br>
+    <label>
+      Quantity:
+      <select name="fries-quantity">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </label>
+  </fieldset>
+  <fieldset>
+    <legend>Garlic Knots</legend>
+    <label>
+      Size:
+      <input type="radio" name="knots-size" value="small">
+      Small
+      <input type="radio" name="knots-size" value="large">
+      Large
+    </label>
+    <br>
+    <label>
+      Quantity:
+      <select name="knots-quantity">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </label>
+  </fieldset>
+  <fieldset>
+    <legend>Breadsticks</legend>
+    <label>
+      Size:
+      <input type="radio" name="breadsticks-size" value="small">
+      Small
+      <input type="radio" name="breadsticks-size" value="large">
+      Large
+    </label>
+    <br>
+    <label>
+      Quantity:
+      <select name="breadsticks-quantity">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </label>
+  </fieldset>
+</form>
